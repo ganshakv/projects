93509b803df0110cbfb0715184da60fff24161d5
ganshakv <ganshashahasad@gmail.com>
2025-07-09 17:49:46 +0530

    first commit

---
+++ script/animation.js
@@ -0,0 +1,166 @@
+// /Heading animation
+
+gsap.to(".contents h3",{
+    y:-15,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".contents h3",
+        scroller: "body",
+            start: "top 10%",
+            end: "top 20%",
+            scrub: 2,
+            duration:1.5,
+    }
+})
+
+gsap.to(".contents h1",{
+    y:-15,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".contents h1",
+        scroller: "body",
+            start: "top 10%",
+            end: "top 60%",
+            scrub: 2,
+            duration:1.5,
+    }
+})
+
+// burger animation starts
+
+
+ScrollTrigger.matchMedia({
+  "(min-width: 768px)": function () {
+gsap.to(".main-burger-wrapper .mid",{
+        y:"35%",
+        scrollTrigger: {
+            trigger:".moving",
+            scroller: "body",
+                start: "top 20%",
+                end: "top 60%",
+                scrub: 1,
+        }
+    })
+
+
+    gsap.to(".main-burger-wrapper .bottom",{
+        y:"100%", 
+        scrollTrigger: {
+            trigger:".moving",
+            scroller: "body",
+                start: "top 20%",
+                end: "top 60%",
+                scrub: 1,
+        }
+    })
+
+// burger animation ends
+
+
+  }
+
+})
+
+ 
+
+// left item animation starts
+
+gsap.from(".left-col .item-img",{
+    x:-200,
+    opacity:0,
+    rotate:360,
+    scrollTrigger: {
+        trigger:".moving",
+        scroller: "body",
+            start: "top 20%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+
+
+gsap.from(".left-col .para",{
+    x:200,
+    y:100,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".moving",
+        scroller: "body",
+            start: "top 20%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+
+// left item animation ends
+
+
+// right item animation starts
+
+gsap.from(".right-col .item-img",{
+    x:200,
+    opacity:0,
+    rotate:360,
+    scrollTrigger: {
+        trigger:".moving",
+        scroller: "body",
+            start: "top 20%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+
+
+gsap.from(".right-col .para",{
+    x:-200,
+    y:100,
+    opacity:0,
+    scrollTrigger: {
+        trigger:".moving",
+        scroller: "body",
+            start: "top 20%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+
+// right item animation ends
+
+
+
+ScrollTrigger.matchMedia({
+  "(min-width: 1024px) and (max-width: 1200px)": function () {
+var tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:".detail-sec",
+        scroller: "body",
+            start: "top 30%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+tl.to(".moving .top",{ top:"115%",left:"100%",scale:1},0)
+tl.to(".moving .mid",{ top:"130%",left:"100%",scale:1},0)
+tl.to(".moving .bottom",{ top:"138%",left:"100%",scale:1},0)
+
+  }
+})
+
+ScrollTrigger.matchMedia({
+  "(min-width: 1440px)": function () {
+var tl=gsap.timeline({
+    scrollTrigger:{
+        trigger:".detail-sec",
+        scroller: "body",
+            start: "top 30%",
+            end: "top 60%",
+            scrub: 2,
+    }
+})
+tl.to(".moving .top",{ top:"117%",left:"100%",scale:0.8},0)
+tl.to(".moving .mid",{ top:"130%",left:"100%",scale:0.8},0)
+tl.to(".moving .bottom",{ top:"138%",left:"100%",scale:0.8},0)
+
+  }
+
+})
---
+++ script/script.js
@@ -0,0 +1,29 @@
+const burger = document.getElementById('burger');
+        const navLinks = document.querySelector('.nav-links');
+
+        burger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        })
+
+
+
+
+
+        // sticky header
+
+
+//         const navbar = document.getElementById('navbar');
+
+// window.addEventListener('scroll', () => {
+// if (window.scrollY > 50) { // Adjust this value based on when you want the color to change
+//     navbar.classList.add('scrolled');
+// } else {
+//     navbar.classList.remove('scrolled');
+// }
+// });
+
+
+
+
+
+
